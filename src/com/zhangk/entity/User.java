fb9baed018ac72635f6326673aa1242ce2c0c4af
zhangk0533 <907144187@qq.com>
2018-11-28 14:31:49 +0800

    20181128

--- src/com/zhangk/entity/User.java
+++ src/com/zhangk/entity/User.java
@@ -8,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.Email;
